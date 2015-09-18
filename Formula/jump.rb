ad2b8c055a8f927035801084de76a15a8deab50a
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-09-18 12:00:28 +0300

    Update the formula for 0.2.0

--- Formula/jump.rb
+++ Formula/jump.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Jump < Formula
   desc "A fuzzy quick-directory jumper."
   homepage "https://github.com/gsamokovarov/jump"
-  url "https://codeload.github.com/gsamokovarov/jump/tar.gz/v0.1.0"
-  sha256 "13b93f7fb9b0163b469c1d1fda6b2ed4918b822e0421be87f81ae865c710d643"
+  url "https://codeload.github.com/gsamokovarov/jump/tar.gz/v0.2.0"
+  sha256 "cd009129c7a2b90eb004df3fe7009b7d62ef48f144f66fb96f741f80f7e5a766"
   head "https://github.com/gsamokovarov/jump.git"
 
   depends_on "go" => :build
