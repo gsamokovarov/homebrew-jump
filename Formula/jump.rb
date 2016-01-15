b77015f53c6bf55e8c5e9e975cad164a3876b5f7
Genadi Samokovarov <gsamokovarov@gmail.com>
2016-01-15 18:43:35 +0200

    Update for 0.5

--- Formula/jump.rb
+++ Formula/jump.rb
@@ -3,34 +3,33 @@ require "language/go"
 class Jump < Formula
   desc "A quick and fuzzy directory jumper."
   homepage "https://github.com/gsamokovarov/jump"
-  url "https://codeload.github.com/gsamokovarov/jump/tar.gz/v0.4.0"
-  sha256 "d105eebf832c6792ea72eaede2e0b7a14bb9da1a9e2b27847a6954c3bb9071d0"
+  url "https://codeload.github.com/gsamokovarov/jump/tar.gz/v0.5.0"
+  sha256 "1f7ac07e62b4ccb33f089e2fdf1269d6c491e7b640f089d321fcce04308edbc5"
   head "https://github.com/gsamokovarov/jump.git"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-    mkdir_p buildpath/"src/github.com/gsamokovarov"
+    (buildpath/"src/github.com/gsamokovarov").mkpath
     ln_s buildpath, buildpath/"src/github.com/gsamokovarov/jump"
     Language::Go.stage_deps resources, buildpath/"src"
 
-    system "go build -o jump"
-    bin.install "jump"
+    system "go", "build", "-o", "#{bin}/jump"
     man1.install "man/jump.1"
   end
 
   def caveats; <<-EOS.undent
-    To install shell integration, follow the instructions at:
-      #{prefix}/bin/jump shell
+    To install the shell integration, follow the instructions at:
+      #{bin}/jump shell
     EOS
   end
 
   test do
-    mkdir_p testpath/"test_dir"
+    (testpath/"test_dir").mkpath
     ENV["JUMP_HOME"] = testpath.to_s
-    system "jump chdir #{testpath/"test_dir"}"
+    system "#{bin}/jump", "chdir", "#{testpath}/test_dir"
 
-    assert_equal testpath/"test_dir", shell_output("jump cd tdir").chomp
+    assert_equal testpath/"test_dir", shell_output("#{bin}/jump cd tdir").chomp
   end
 end
